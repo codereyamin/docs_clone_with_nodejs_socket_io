ad3d6b21d42f4226b4d3a2c85f3e9b2578ac9b1c
codereyamin <codereyamin@gmail.com>
2023-04-07 16:34:09 +0600

    designing document page

--- lib/screens/document_screen/document_screen.dart
+++ lib/screens/document_screen/document_screen.dart
@@ -1,4 +1,6 @@
+import 'package:docs_clone_with_nodejs_socket_io/utils/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart' as quill;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class DocumentScreen extends ConsumerStatefulWidget {
@@ -10,10 +12,93 @@ class DocumentScreen extends ConsumerStatefulWidget {
 }
 
 class _DocumentScreenState extends ConsumerState<DocumentScreen> {
+  TextEditingController titleTextEditingController = TextEditingController(text: "unTitle");
+  final quill.QuillController _controller = quill.QuillController.basic();
+
+  @override
+  void dispose() {
+    titleTextEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text(widget.id)),
+      backgroundColor: cWhiteColor,
+      appBar: AppBar(
+        backgroundColor: cWhiteColor,
+        elevation: 0,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.lock,
+                size: 16,
+              ),
+              label: const Text("Share"),
+              style: ElevatedButton.styleFrom(backgroundColor: cBlueColor),
+            ),
+          ),
+        ],
+        title: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 9.0),
+          child: Row(children: [
+            Image.asset(
+              "asset/images/docs-logo.png",
+              height: 40,
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            SizedBox(
+                width: 300,
+                child: TextField(
+                  controller: titleTextEditingController,
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: cBlueColor),
+                    ),
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.only(left: 10),
+                  ),
+                ))
+          ]),
+        ),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(1),
+          child: Container(
+            decoration: BoxDecoration(border: Border.all(color: cGrayColor, width: 0.1)),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            quill.QuillToolbar.basic(controller: _controller),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: SizedBox(
+                width: 700,
+                child: Card(
+                  color: cWhiteColor,
+                  elevation: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(30.0),
+                    child: quill.QuillEditor.basic(
+                      controller: _controller,
+                      readOnly: false, // true for view only mode
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const Color cWhiteColor = Colors.white;
 const Color cBlackColor = Colors.black;
 const Color cRedColor = Colors.red;
+var cGrayColor = Colors.green.shade800;
+const Color cBlueColor = Color.fromRGBO(26, 115, 232, 1);
